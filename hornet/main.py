12241ba1e809fe222d3016283051b2288588178f
Aniket Panse <aniketpanse@gmail.com>
2014-02-11 01:44:47 +0530

    added a basic skeleton for the project

--- hornet/main.py
+++ hornet/main.py
@@ -17,8 +17,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
+import logging
+import os
+import shutil
+
+import hornet
+
 
 class Hornet(object):
 
-    def __init__(self, config=None):
+    def __init__(self):
+        self.port = None
+        self.server = None
+        self.load_config()
+
+    def load_config(self):
+        if not os.path.isfile('config.json'):
+            source = os.path.join(os.path.dirname(hornet.__file__), 'data', 'default_config.json')
+            destination = os.path.join(os.getcwd(), 'config.json')
+            logging.debug('Config file not found, copying default'.format(source))
+            shutil.copyfile(src=source, dst=destination)
+        with open('config.json', 'r') as config_fp:
+            config = json.load(config_fp)
+        self.port = config['port']
+
+    def start(self):
         pass
