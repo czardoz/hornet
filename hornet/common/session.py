f22dde4b4da663c67fe8452ec4393119f6340500
Aniket Panse <aniket@netskope.com>
2014-04-08 16:50:15 +0530

    basic session class

---
+++ hornet/common/session.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+#
+# Hornet - SSH Honeypot
+#
+# Copyright (C) 2014 Aniket Panse <aniketpanse@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import uuid
+import arrow
+
+
+class Session(object):
+
+    def __init__(self, client_address):
+        self.id = uuid.uuid4()
+        self.start_time = arrow.now().timestamp
+        self.end_time = None
+        self.live = True
