4cb01148cbdc81d1ed151b2a73c0fc09894ea7b4
Aniket Panse <aniket@netskope.com>
2014-04-08 17:52:09 +0530

    ability to authenticate usernames and passwords

--- hornet/core/handler.py
+++ hornet/core/handler.py
@@ -18,15 +18,77 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import os
+import gevent
+from paramiko import SSHException
+
+from telnetsrv.paramiko_ssh import SSHHandler, getRsaKeyFile
+from hornet.common.session import Session
+from hornet.common.shell import Shell
 
 logger = logging.getLogger(__name__)
 
 
-class SSHHandler(object):
+class SSHWrapper(object):
 
-    def __init__(self, vhosts, sessions):
+    """ Helper class to pass the client socket to _SSHHandler """
+
+    def __init__(self, vhosts, sessions, config):
         self.vhosts = vhosts
         self.sessions = sessions
+        self.config = config
 
     def handle_session(self, client_socket, client_address):
-        logger.info('Connection from {}'.format(client_address))
+        current_session = Session(client_address)
+        self.sessions[current_session.id] = current_session
+        logger.info('Connection from {}, {}'.format(client_address, client_socket))
+
+        # Set the host_key attribute on our _SSHHandler class
+        key_file_path = os.path.join(os.getcwd(), self.config.key_file)
+        _SSHHandler.host_key = getRsaKeyFile(key_file_path)
+
+        try:
+            _SSHHandler(current_session, client_socket, client_address)
+        except SSHException:
+            logging.error('SSH Session {} ended unexpectedly'.format(current_session.id))
+
+
+class _SSHHandler(SSHHandler):
+    """
+    Wraps the telnetsrv paramiko module to fit the Honeypot architecture.
+    """
+
+    telnet_handler = Shell
+
+    def __init__(self, session, socket, client_address):
+        self.session = session
+        request = _SSHHandler.dummy_request()
+        request._sock = socket
+        super(_SSHHandler, self).__init__(request, client_address, None)
+
+    def authCallbackUsername(self, username):
+        #make sure no one can logon
+        raise
+
+    def authCallback(self, username, password):
+        logger.info('Login attempt: {} -- {}'.format(username, password))
+        return True
+
+    def setup(self):
+
+        self.transport.load_server_moduli()
+        self.transport.add_server_key(self.host_key)
+        self.transport.start_server(server=self)
+
+        while True:
+            channel = self.transport.accept(20)
+            if channel is None:
+                # check to see if any thread is running
+                any_running = False
+                for c, thread in self.channels.items():
+                    if thread.is_alive():
+                        any_running = True
+                        break
+                if not any_running:
+                    break
+            gevent.sleep(0)
