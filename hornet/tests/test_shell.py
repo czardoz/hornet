36e9f2bf426d00c23a2a3ef4bfb961f1fcfd6bfc
Aniket Panse <aniketpanse@gmail.com>
2014-05-06 17:08:40 +0530

    added a test for the shell initialization and set_host method

---
+++ hornet/tests/test_shell.py
@@ -0,0 +1,72 @@
+# !/usr/bin/env python
+#
+# Hornet - SSH Honeypot
+#
+# Copyright (C) 2014 Aniket Panse <aniketpanse@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gevent.monkey
+import paramiko
+from hornet.main import Hornet
+
+gevent.monkey.patch_all()
+
+import os
+import shutil
+import unittest
+import tempfile
+
+import hornet
+
+
+class HornetTests(unittest.TestCase):
+
+    def setUp(self):
+        self.working_dir = tempfile.mkdtemp()
+        test_config = os.path.join(os.path.dirname(hornet.__file__), 'data', 'default_config.json')
+        shutil.copyfile(test_config, os.path.join(self.working_dir, 'config.json'))
+
+    def tearDown(self):
+        shutil.rmtree(self.working_dir)
+
+    def test_shell_set_host(self):
+        """ Tests if host related attributes are set on the shell properly """
+
+        honeypot = Hornet(self.working_dir)
+        honeypot.start()
+        while honeypot.server.server_port == 0:  # wait until the server is ready
+            gevent.sleep(0)
+        port = honeypot.server.server_port
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # If we log in properly, this should raise no errors
+        client.connect('127.0.0.1', port=port, username='testuser', password='testpassword')
+        channel = client.invoke_shell()
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        welcome = ''
+        while channel.recv_ready():
+            welcome += channel.recv(1)
+        lines = welcome.split('\r\n')
+        prompt = lines[-1]
+        username, remaining = prompt.split('@')
+        self.assertEquals(username, 'testuser')
+        hostname, remaining = remaining.split(':')
+        self.assertEquals(hostname, 'test02')
+        self.assertTrue(prompt.endswith('$ '))
+        honeypot.stop()
+
