064586f761f9929deb6acd548a573fb9e9ae0ded
Aniket Panse <aniketpanse@gmail.com>
2014-08-08 21:51:02 +0530

    moved the echo tests to the right place

--- hornet/tests/test_shell.py
+++ hornet/tests/test_shell.py
@@ -329,3 +329,183 @@ class HornetTests(unittest.TestCase):
         channel.send('logout\r\n')
 
         honeypot.stop()
+
+    def test_echo_params(self):
+        """ Tests if echo command works when parameters are specified """
+
+        honeypot = Hornet(self.working_dir)
+        honeypot.start()
+        while honeypot.server.server_port == 0:  # wait until the server is ready
+            gevent.sleep(0)
+        port = honeypot.server.server_port
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # If we log in properly, this should raise no errors
+        client.connect('127.0.0.1', port=port, username='testuser', password='testpassword')
+        channel = client.invoke_shell()
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        welcome = ''
+        while channel.recv_ready():
+            welcome += channel.recv(1)
+        lines = welcome.split('\r\n')
+        prompt = lines[-1]
+        self.assertTrue(prompt.endswith('$ '))
+
+        # Now send the echo command
+        channel.send('echo this is a test\r\n')
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        output = ''
+        while not output.endswith('$ '):
+            output += channel.recv(1)
+
+        lines = output.split('\r\n')
+        command = lines[0]
+        command_output = '\r\n'.join(lines[1:-1])
+        next_prompt = lines[-1]
+        self.assertEquals('echo this is a test', command)
+        self.assertEquals('this is a test', command_output)
+        self.assertTrue(next_prompt.endswith('$ '))
+        honeypot.stop()
+
+    def test_echo_no_params(self):
+        """ Tests if echo command works when parameters are specified """
+
+        honeypot = Hornet(self.working_dir)
+        honeypot.start()
+        while honeypot.server.server_port == 0:  # wait until the server is ready
+            gevent.sleep(0)
+        port = honeypot.server.server_port
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # If we log in properly, this should raise no errors
+        client.connect('127.0.0.1', port=port, username='testuser', password='testpassword')
+        channel = client.invoke_shell()
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        welcome = ''
+        while channel.recv_ready():
+            welcome += channel.recv(1)
+        lines = welcome.split('\r\n')
+        prompt = lines[-1]
+        self.assertTrue(prompt.endswith('$ '))
+
+        # Now send the echo command
+        channel.send('echo\r\n')
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        output = ''
+        while not output.endswith('$ '):
+            output += channel.recv(1)
+
+        lines = output.split('\r\n')
+        command = lines[0]
+        command_output = '\r\n'.join(lines[1:-1])
+        next_prompt = lines[-1]
+        self.assertEquals('echo', command)
+        self.assertEquals('', command_output)
+        self.assertTrue(next_prompt.endswith('$ '))
+        honeypot.stop()
+
+    def test_echo_env_var(self):
+        """ Tests if echo command works when environment variables as specified in the
+            config are specified """
+
+        honeypot = Hornet(self.working_dir)
+        honeypot.start()
+        while honeypot.server.server_port == 0:  # wait until the server is ready
+            gevent.sleep(0)
+        port = honeypot.server.server_port
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # If we log in properly, this should raise no errors
+        client.connect('127.0.0.1', port=port, username='testuser', password='testpassword')
+        channel = client.invoke_shell()
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        welcome = ''
+        while channel.recv_ready():
+            welcome += channel.recv(1)
+        lines = welcome.split('\r\n')
+        prompt = lines[-1]
+        self.assertTrue(prompt.endswith('$ '))
+
+        # Now send the echo command
+        channel.send('echo $BROWSER\r\n')
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        output = ''
+        while not output.endswith('$ '):
+            output += channel.recv(1)
+
+        lines = output.split('\r\n')
+        command = lines[0]
+        command_output = '\r\n'.join(lines[1:-1])
+        next_prompt = lines[-1]
+        self.assertEquals('echo $BROWSER', command)
+        self.assertEquals('firefox', command_output)
+        self.assertTrue(next_prompt.endswith('$ '))
+        honeypot.stop()
+
+    def test_echo_star(self):
+        """ Tests if echo command works when '*' exists in the params """
+
+        honeypot = Hornet(self.working_dir)
+        honeypot.start()
+        default_host = honeypot.vhosts[honeypot.config.default_hostname]
+        default_host.filesystem.makedir('/etc')
+        default_host.filesystem.makedir('/var')
+        default_host.filesystem.makedir('/opt')
+
+        while honeypot.server.server_port == 0:  # wait until the server is ready
+            gevent.sleep(0)
+        port = honeypot.server.server_port
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # If we log in properly, this should raise no errors
+        client.connect('127.0.0.1', port=port, username='testuser', password='testpassword')
+        channel = client.invoke_shell()
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        welcome = ''
+        while channel.recv_ready():
+            welcome += channel.recv(1)
+        lines = welcome.split('\r\n')
+        prompt = lines[-1]
+        self.assertTrue(prompt.endswith('$ '))
+
+        # Now send the echo command
+        channel.send('echo *\r\n')
+
+        while not channel.recv_ready():
+            gevent.sleep(0)  # :-(
+
+        output = ''
+        while not output.endswith('$ '):
+            output += channel.recv(1)
+
+        lines = output.split('\r\n')
+        command = lines[0]
+        command_output = '\r\n'.join(lines[1:-1])
+        next_prompt = lines[-1]
+        self.assertEquals('echo *', command)
+        self.assertTrue('var' in command_output)
+        self.assertTrue('etc' in command_output)
+        self.assertTrue('opt' in command_output)
+        self.assertTrue(next_prompt.endswith('$ '))
+        honeypot.stop()
