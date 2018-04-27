98c91bc81661bc87131410bd156efeee2233d5c4
Aniket Panse <aniketpanse@gmail.com>
2018-04-26 23:40:16 -0500

    fixed an issue where docker process would start in the wrong directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,10 +5,11 @@ mysql -u root -v -e 'CREATE DATABASE IF NOT EXISTS hornet;';
 mysql -u root -v -e "CREATE USER 'hornetservice'@'localhost';";
 mysql -u root -v -e "GRANT ALL PRIVILEGES ON *.* TO 'hornetservice'@'localhost';";
 
-sed 's/\/\/travis\@/\/\/hornetservice\@/' ./hornet/data/default_config.json > config.json
-sed -i 's/port": 0,/port":\ 2222,/' config.json
-sed -i 's/"host": "127.0.0.1",/"host": "0.0.0.0",/' config.json
+mkdir /opt/vfs/;
+
+sed 's/\/\/travis\@/\/\/hornetservice\@/' ./hornet/data/default_config.json > /opt/vfs/config.json;
+sed -i 's/port": 0,/port":\ 2222,/' /opt/vfs/config.json;
+sed -i 's/"host": "127.0.0.1",/"host": "0.0.0.0",/' /opt/vfs/config.json;
 
 mkdir -p /var/log/supervisord/;
-mkdir /opt/vfs/;
 supervisord -n -c ./hornet-supervisord.conf;
