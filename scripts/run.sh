576eede03d898b00f459ff5bf8ce08c86e7b912b
Aniket Panse <aniketpanse@gmail.com>
2018-04-27 01:08:29 -0500

    ensure that db user has permissions

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 /etc/init.d/mysql restart;
 mysql -u root -v -e 'CREATE DATABASE IF NOT EXISTS hornet;';
 mysql -u root -v -e "CREATE USER 'hornetservice'@'%';";
-mysql -u root -v -e "GRANT ALL PRIVILEGES ON *.* TO 'hornetservice'@'localhost';";
+mysql -u root -v -e "GRANT ALL PRIVILEGES ON *.* TO 'hornetservice'@'%';";
 
 mkdir /opt/vfs/;
 
