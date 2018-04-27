a751ed23f81de35c07a99479d817bcb253faa206
Aniket Panse <aniketpanse@gmail.com>
2018-04-27 00:54:08 -0500

    ensure that mysql inside docker can be connected to

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 /etc/init.d/mysql restart;
 mysql -u root -v -e 'CREATE DATABASE IF NOT EXISTS hornet;';
-mysql -u root -v -e "CREATE USER 'hornetservice'@'localhost';";
+mysql -u root -v -e "CREATE USER 'hornetservice'@'%';";
 mysql -u root -v -e "GRANT ALL PRIVILEGES ON *.* TO 'hornetservice'@'localhost';";
 
 mkdir /opt/vfs/;
